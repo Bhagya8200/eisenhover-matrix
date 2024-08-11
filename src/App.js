d6edb6e1285bac8167ef854a2b35d7fd7cbe3959
bhagya <neelapubhagyasree8200@gmail.com>
2024-08-11 23:09:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ToDoList from "./ToDoList/ToDoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <ToDoList /> */}
+      <div className="wrapper">
+        <h1>Prioritize your tasks</h1>
+        <div className="eisenhover-matrix">
+          <div></div>
+          <div>
+              URGENT
+            
+          </div>
+          <div>
+            <p>NOT URGENT</p>
+          </div>
+          <div>
+            <p>IMPORTANT</p>
+          </div>
+          <div className="do_it">
+            <h1 className="do_it_heading">DO IT</h1>
+          </div>
+
+          <div className="schedule_it">
+          <h1 className="schedule_it_heading">SCHEDULE IT</h1>
+          </div>
+          <div>
+            <pre>
+            NOT IMPORTANT
+            </pre>
+          </div>
+          
+          <div className="delegate_it">
+          <h1 className="delegate_it_heading">DELEGATE IT</h1>
+          </div>
+          <div className="delete_it">
+          <h1 className="delete_it_heading">DELETE IT</h1>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
